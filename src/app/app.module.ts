052bb8f05471ae72ce72e7d970cad5de8e5ff6de
Ismaila Sacko <ismalsacko@yahoo.fr>
2022-10-01 07:27:02 +0200

    Mise en place de components et et le style de pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { ContainerComponent } from './components/container/container.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    AsideComponent,
+    ContainerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
